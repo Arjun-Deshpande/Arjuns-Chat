2f1e3dba4b50e4997c0181eed7f6bb8f59f438c3
Arjun <73472482+Arjun-Deshpande@users.noreply.github.com>
2022-03-24 19:51:40 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+/*import React, { useContext } from "react";
 
 import { Context } from "../context";
 
@@ -63,4 +63,4 @@ export default function Auth(){
       </div>
     </div>
   );
-};
+/*};
